ee82d32ffa49117d068ab8e8eb6ab78ecd77c90c
Andy Wilson <andy@tx.net>
2009-11-13 14:22:21 -0600

    Color theme tweak

--- ajw-color-config.el
+++ ajw-color-config.el
@@ -6,8 +6,11 @@
 ;;; If on Windows, use Taylor
 ;;; If on console/tty, use clarity
 (if window-system
-    (if (eq system-type 'windows-nt) 
+   (if (eq system-type 'windows-nt) 
 	(list (color-theme-taylor)
 	      (set-face-background 'bold "unspecified"))
-     (color-theme-tango))
+        (color-theme-tango-darkbg))
     (color-theme-clarity))
+
+;;; Class it up a bit with some not-quite-black
+(set-background-color "gray10")
