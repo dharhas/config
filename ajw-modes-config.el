1b68e4e41ef4b13d4eb74232f195c8a522a30c37
Andy Wilson <andy@tx.net>
2009-12-01 12:17:55 -0600

    Added flyspell prog mode (spellchecking on comments) and a mail notification icon that might come in handy...

--- ajw-modes-config.el
+++ ajw-modes-config.el
@@ -58,6 +58,8 @@
 
 
 ;;; flyspell
+(require 'flyspell)
+(flyspell-prog-mode)
 ;ispell-program-name is defined in OS-specific files
 ;(setq ispell-program-name "c:\\Program Files\\Aspell\\bin\\aspell")
 
@@ -69,6 +71,24 @@
 
 (add-hook 'MH-Letter
 	  (lambda () (flymake-mode 1)))
+
+;;; Turn on display time mode for mail notification
+(setq display-time-format "")  ; don't need time string in modeline
+
+(defface display-time-mail-face '((t (:background "red")))
+    "If display-time-use-mail-icon is non-nil its background
+     color is that of this face. Should be disting from
+     mode-line. Note that thtis does not seem to affect
+     display-time-mail-string as claimed.")
+
+(setq
+ display-time-mail-file "/var/mail/wilsaj"
+ display-time-use-mail-icon nil
+ display-time-mail-face 'display-time-mail-face
+ display-time-interval 10)
+
+(display-time-mode t)
+
 
 
 
