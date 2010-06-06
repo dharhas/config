4b083c6fe966f67c49ed6aafeaee750f8f6bdd61
Andy Wilson <andy@tx.net>
2010-06-06 06:59:51 -0500

    Generalized batt.sh to work on arch linux

--- .config/awesome/batt.sh
+++ .config/awesome/batt.sh
@@ -2,12 +2,26 @@
 #
 # Battery status script for widget
 #
-if [ `acpiconf -i 0 | grep "State:" | awk '{print $2}'` = 'charging' ]; then
-    BATTERY_SIGN='+'
+
+if [ `uname` = 'Linux' ]; then
+    # Linux batter check
+    if [ `cat /sys/class/power_supply/BAT0/status` = 'Charging' ]; then
+        BATTERY_SIGN='+'
+      else
+        BATTERY_SIGN='-'
+    fi
+
+    BATTERY_PERCENT=$(expr $(expr `cat /sys/class/power_supply/BAT0/charge_now` \* 100) / `cat /sys/class/power_supply/BAT0/charge_full`)
   else
-    BATTERY_SIGN='-'
+    # FreeBSD battery check
+    if [ `acpiconf -i 0 | grep "State:" | awk '{print $2}'` = 'charging' ]; then
+        BATTERY_SIGN='+'
+      else
+        BATTERY_SIGN='-'
+    fi
+
+    BATTERY_PERCENT=`acpiconf -i 0 | grep "Remaining capacity:" | awk '{print $3}'`
 fi
 
-BATTERY_PERCENT=`acpiconf -i 0 | grep "Remaining capacity:" | awk '{print $3}'`
 
 echo ${BATTERY_SIGN} ${BATTERY_PERCENT}
