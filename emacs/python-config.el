b7f2427de20a1b6bc374d6c201bf6fe79cde5e36
andy wilson <wilson.andrew.j+github@gmail.com>
2010-11-08 12:31:47 -0600

    explicitly set python path and pymacs path to python2 for now

--- emacs/python-config.el
+++ emacs/python-config.el
@@ -6,6 +6,10 @@
 (add-to-list 'auto-mode-alist '("\\.py\\'" . python-mode))
 (add-to-list 'interpreter-mode-alist '("python" . python-mode))
 
+;;; set python path explicitly to python2 for now
+(setq py-python-command "/usr/bin/python2")
+(setenv "PYMACS_PYTHON" "/usr/bin/python2")
+
 ;;; ipython
 ;;; XXX: for global .emacs file: seperate out the pylab backend args
 ;;;      (e.g. -wthread) per OS
@@ -27,11 +31,10 @@
 ;;; Ropemacs
 (pymacs-load "ropemacs" "rope-")
 
-;; NOTE: this lazy-loads pymacs. Replace the line above with the
+;; NOTE: this lazy-loads ropemacs. Replace the line above with the
 ;; commented code below if slow startup is an issue.
 ;;
 ;; (eval-after-load "python-mode"
 ;;   '(progn
 ;;     (pymacs-load "ropemacs" "rope-")
 ;;   ))
-
