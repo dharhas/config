286936b2de1f73403986c8803108b63922110101
Andy Wilson <wilson.andrew.j@gmail.com>
2011-01-04 10:26:31 -0600

    add some emacs voodoo to keep tramp from hanging

--- emacs/modes-config.el
+++ emacs/modes-config.el
@@ -75,7 +75,11 @@
 
 ;;; tramp
 (setq tramp-default-method "ssh")
+(setq tramp-shell-prompt-pattern "^[^$>\n]*[#$%>] *\\(\[[0-9;]*[a-zA-Z] *\\)*")
 
+;; for debugging tramp
+;(setq tramp-verbose 10)
+;(setq tramp-debug-buffer t
 
 ;(add-to-list 'tramp-default-proxies-alist
 ;             '(nil "\\`root\\'" "/ssh:%h:"))
